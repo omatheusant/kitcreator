6cbcdf205926fd699715a61731c6035deaa4afc8
Matheus Santana <santalvesmatheus@gmail.com>
2023-12-12 16:38:29 -0300

    create bringToFront with dbclick

--- src/App.jsx
+++ src/App.jsx
@@ -11,69 +11,86 @@ import { Background } from './components/Tools/Background';
 import { Eraser } from './components/Tools/Eraser';
 
 function App() {
-  const [canvas, setCanvas] = useState(null);
-  const [size, setSize] = useState({ width: 1700, height: 1200 });
+ const [canvas, setCanvas] = useState(null);
+ const [size, setSize] = useState({ width: 1700, height: 1200 });
 
+ const initCanvas = () => {
+   if (canvas) {
+     canvas.dispose();
+   }
+   const newCanvas = new fabric.Canvas('canvas', {
+     height: size.height,
+     width: size.width,
+     backgroundColor: 'white'
+   });
+   setCanvas(newCanvas);
+ };
 
-  const initCanvas = () => {
-    if (canvas) {
-      canvas.dispose();
-    }
-    const newCanvas = new fabric.Canvas('canvas', {
-      height: size.height,
-      width: size.width,
-      backgroundColor: 'white'
-    });
-    setCanvas(newCanvas);
-  };
+ useEffect(() => {
+   initCanvas();
+ }, [size]);
 
-  useEffect(() => {
-    initCanvas();
-  }, [size]);
+ useEffect(() => {
+   if (canvas) {
+     const handleKeyDown = (e) => {
+       if (e.key === 'Delete') {
+         const activeObjects = canvas.getActiveObjects();
+         if (activeObjects.length > 0) {
+           canvas.discardActiveObject();
+           canvas.remove(...activeObjects);
+           canvas.requestRenderAll();
+         }
+       }
+       if (e.ctrlKey && e.key === 'z') {
+         canvas.undo();
+       }
+       if (e.ctrlKey && e.key === 'y') {
+         canvas.redo();
+       }
+     };
 
-  useEffect(() => {
-    if (canvas) {
-      const handleKeyDown = (e) => {
-        if (e.key === 'Delete') {
-          const activeObjects = canvas.getActiveObjects();
-          if (activeObjects.length > 0) {
-            canvas.discardActiveObject();
-            canvas.remove(...activeObjects);
-            canvas.requestRenderAll();
-          }
-        }
-        if (e.ctrlKey && e.key === 'z') {
-          canvas.undo();
-        }
-        if (e.ctrlKey && e.key === 'y') {
-          canvas.redo();
-        }
-      };
+     window.addEventListener('keydown', handleKeyDown);
 
-      window.addEventListener('keydown', handleKeyDown);
+     return () => {
+       window.removeEventListener('keydown', handleKeyDown);
+     };
+   }
+ }, [canvas]);
 
-      return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-      };
-    }
-  }, [canvas]);
+ useEffect(() => {
+   if (canvas) {
+     const handleDoubleClick = (e) => {
+       const target = canvas.findTarget(e);
+       if (target) {
+         canvas.bringToFront(target);
+         canvas.requestRenderAll();
+       }
+     };
 
-  return (
-    <>
-      <Header canvas={canvas} />
-      <SideBar>
-        <Resize setSize={setSize} />
-        <AddText canvas={canvas} />
-        <AddImage canvas={canvas} />
-        <Eraser canvas={canvas} />
-        <Background canvas={canvas} />
-      </SideBar>
+     canvas.on('mouse:dblclick', handleDoubleClick);
 
-      <div className='w-full mt-10 flex justify-center z-0'>
-        <canvas id="canvas"></canvas>
-      </div>
-    </>
-  );
+     return () => {
+       canvas.off('mouse:dblclick', handleDoubleClick);
+     };
+   }
+ }, [canvas]);
+
+ return (
+   <>
+     <Header canvas={canvas} />
+     <SideBar>
+       <Resize setSize={setSize} />
+       <AddText canvas={canvas} />
+       <AddImage canvas={canvas} />
+       <Eraser canvas={canvas} />
+       <Background canvas={canvas} />
+     </SideBar>
+
+     <div className='w-full mt-10 flex justify-center z-0'>
+       <canvas id="canvas"></canvas>
+     </div>
+   </>
+ );
 }
 
 export default App;
